bf42a9b5d62894819a87bc5d9096027ff55de5f9
Kayla Firestack <jj.dev@kaylafire.me>
2026-05-29 13:14:11 -0400

    feat(flake): update nixpkgs to use locking tarball

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Build a cargo project";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "https://channels.nixos.org/nixos-unstable/nixexprs.tar.xz";
 
     flake-utils.url = "github:numtide/flake-utils";
   };
